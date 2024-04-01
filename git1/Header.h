39744613b1da1be0144b69f3c241a31235d3d354
marl_123@mail.ru <marl_123@mail.ru>
2024-04-01 20:33:49 +0300

    Добавили заголовочный файл

---
+++ git1/Header.h
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+void func(int n) {
+	for (int i = 0; i < n; i++)
+	{
+		cout << i << endl;
+	}
+}
--- git1/git1.cpp
+++ git1/git1.cpp
@@ -1,11 +1,13 @@
 ﻿
 #include <iostream>
+#include "Header.h"
 using namespace std;
 
 int main()
 {
     int a = 5;
     cout << a;
+    func(a);
 }
 
 
